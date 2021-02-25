e7155a93a6eae8167198e3bf52893ae612b9c255
2044233868@qq.com <2044233868@qq.com>
2021-02-25 16:41:39 +0800

    first-commit

---
+++ atguigu/src/main/java/com/poi/data/structure/and/algorithm/atguigu/AtguiguApplication.java
@@ -0,0 +1,13 @@
+package com.poi.data.structure.and.algorithm.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtguiguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtguiguApplication.class, args);
+    }
+
+}
---
+++ atguigu/src/test/java/com/poi/data/structure/and/algorithm/atguigu/AtguiguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poi.data.structure.and.algorithm.atguigu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtguiguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
