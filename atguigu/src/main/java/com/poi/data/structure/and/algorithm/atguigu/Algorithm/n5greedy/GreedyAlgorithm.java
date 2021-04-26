e835341e0acf61b05a9ab00467dda8d82f1002d6
Administrator <2044233868@qq.com>
2021-04-26 23:15:34 +0800

    第十四章 十大算法 克鲁斯卡尔

---
+++ atguigu/src/main/java/com/poi/data/structure/and/algorithm/atguigu/Algorithm/n5greedy/GreedyAlgorithm.java
@@ -0,0 +1,9 @@
+package com.poi.data.structure.and.algorithm.atguigu.Algorithm.n5greedy;
+
+/**
+ * @author poi 2021/4/26 20:38
+ * @version 1.0
+ * 2021/4/26 20:38
+ */
+public class GreedyAlgorithm {
+}
---
+++ atguigu/src/main/java/com/poi/data/structure/and/algorithm/atguigu/Algorithm/n6prim/PrimAlgorithm.java
@@ -0,0 +1,9 @@
+package com.poi.data.structure.and.algorithm.atguigu.Algorithm.n6prim;
+
+/**
+ * @author poi 2021/4/26 20:59
+ * @version 1.0
+ * 2021/4/26 20:59
+ */
+public class PrimAlgorithm {
+}
